20db8c70b7a199521e94b8a17c5960cd9a4dd699
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2023-10-18 11:02:54 +0530

    added tooltips in price range slider values

--- app/src/Pages/Home/components/Elements/PriceRange.jsx
+++ app/src/Pages/Home/components/Elements/PriceRange.jsx
@@ -4,15 +4,41 @@ import {
   RangeSliderTrack,
   RangeSliderFilledTrack,
   RangeSliderThumb,
+  Tooltip
 } from '@chakra-ui/react'
 const PriceRange = () => {
+  const [sliderMinValue, setsliderMinValue] = React.useState(0)
+  const [sliderMaxValue, setsliderMaxValue] = React.useState(0)
+  const [showTooltip, setShowTooltip] = React.useState(false)
   return (
-    <RangeSlider onChange={(e) => {console.log(e)}} aria-label={['min', 'max']} defaultValue={[0, 1000]}>
+    <RangeSlider onChange={(v) => {setsliderMinValue(v[0]); setsliderMaxValue(v[1])}} aria-label={['min', 'max']} defaultValue={[0, 1000]}
+    onMouseEnter={() => setShowTooltip(true)}
+    onMouseLeave={() => setShowTooltip(false)}>
   <RangeSliderTrack bg='lightgrey'>
     <RangeSliderFilledTrack />
   </RangeSliderTrack>
+  <Tooltip isRound={true}
+        hasArrow
+        bg='blue.500'
+        color='white'
+        placement='top'
+        isOpen={showTooltip}
+        label={`${sliderMinValue}K`}
+        borderRadius='xl'
+      >
   <RangeSliderThumb index={0} />
+      </Tooltip>
+  <Tooltip
+        hasArrow
+        bg='blue.500'
+        color='white'
+        placement='top'
+        isOpen={showTooltip}
+        label={`${sliderMaxValue}K`}
+        borderRadius='xl'
+      >
   <RangeSliderThumb index={1} />
+      </Tooltip>
 </RangeSlider>
   )
 }
